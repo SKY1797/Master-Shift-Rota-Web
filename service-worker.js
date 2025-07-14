9d30870a3670c81d834eb08c5eadf5c76a6aa36a
AKASH KANTARIA <68785054+SKY1797@users.noreply.github.com>
2025-07-14 16:27:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -18,6 +18,18 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      )
+    )
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
